9af5705aaeb9ebcf14656f21fe721ff1850bf319
MARASI KEPHA <thecloud79@gmail.com>
2024-12-08 15:51:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 
 Route::get('/register',function()
                 {
-                return view(auth.register');
+                return view('auth.register');
                 });
 
 
