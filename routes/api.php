d3b99ce2a8fd9751dd7648e1b7377eab2b50a675
MARASI KEPHA <thecloud79@gmail.com>
2024-12-08 16:25:00 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\DriverController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user(); 
 });
+
+Route::get('/drivers', [DriverController::class, 'show']);   // List all users
+Route::get('/welcome', function(){
+    return view('/welcome');
+}); // Get a specific user
+Route::post('/users', [UserController::class, 'store']);  // Create a new user
+Route::put('/users/{id}', [UserController::class, 'update']); // Update a specific user
+Route::delete('/users/{id}', [UserController::class, 'destroy']); // Delete a specific user
+
