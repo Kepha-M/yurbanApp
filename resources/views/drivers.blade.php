c9f395034bcf27f4a3440f9649523ef41e5513aa
MARASI KEPHA <thecloud79@gmail.com>
2024-12-09 22:42:37 +0300

    Update drivers.blade.php

--- resources/views/drivers.blade.php
+++ resources/views/drivers.blade.php
@@ -10,19 +10,6 @@
 <div class="bg-gray-50 text-black/100 dark:bg-black dark:text-white/0">
 <div class="table-responsive">
     <h6>All registered 2024 Drivers</h6><br>
-    <label>Total:-{{{DB::table ('drivers')->count()}}}</label>
-
-    
-        <div class="row align-content-center">
-            <form method="POST" action="">
-                <label class="form-label h-20">County:</label>
-                <select > 
-                    @foreach ($drivers as $location)
-                    <option value="{{$location->location}}">{{$location->location}}</option>
-                        
-                    @endforeach
-            </form>
-        </div>
 
     <table class="table table-striped table-hover">
         <thead>
@@ -35,34 +22,7 @@
             <th>County</th>
         </tr>
         </thead>
-        <tbody>
-            @foreach($drivers as  $list)
-            
-            <tr>
-                <td class="py-1"> 
-                    {{$list->id}}
-                </td>
-                <td>
-                    {{$list->id_number}}
-                </td>
-                <td>
-                    {{$list->name}}
-                </td>
-                <td>
-                    
-                        {{$list->email}}
-                    
-                </td>
-                <td>
-                    
-                    {{$list->phone_number}}
-                
-            </td>
-            <td>{{$list->location}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+      </table>
 </div>
 </div>
 @endsection
